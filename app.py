2d7c56ee9b5d31df049c916c4b6078a7712a3c8d
brywal <57795509+brywal@users.noreply.github.com>
2021-06-19 21:37:43 +0200

    init

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify, request
+import numpy as np
+import joblib
+import traceback
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET'])
+def hello():
+    return "hello"
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    model = joblib.load("model.pkl")
+    if model:
+        try:
+            json = request.get_json()
+            temp = list(json[0].values())
+            values = np.array(temp)
+            prediction = model.predict(values)
+            print("here:", prediction)
+            return jsonify({'prediction': str(prediction[0])})
+        except:
+            return jsonify({'trace': traceback.format_exc()})
+    else:
+        return 'No model here to use'
+
+
+if __name__ == '__main__':
+    app.run()
